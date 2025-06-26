4b5f70a6f890b0a2b9f1682930e80c73b780e18d
yuzili <395345203@qq.com>
2025-06-26 23:24:37 +0800

    feat: getUserInfo.

---
+++ src/common/utils.ts
@@ -0,0 +1,8 @@
+// export async function getUserInfo(mid: number): Promise<UserInfo> {
+//   try {
+//     const userInfo = (await useApi.getInfo(mid)) || {}
+//   }
+//   catch (error) {
+
+//   }
+// }
---
+++ src/tools/user.ts
@@ -0,0 +1,17 @@
+// export function registerUserTools(server: McpServer): void {
+//   server.tool(
+//     'get_user_info',
+//     'get information about a bilibili user',
+//     {
+//       mid: z.number().int().positive().describe('user\'s identification ID'),
+//     },
+//     async ({ mid }) => {
+//       try {
+
+//       }
+//       catch (error) {
+
+//       }
+//     },
+//   )
+// }
